e3d3cae3a1c957803b7337168078fdc7513a3dce
Felipe <felipesalaberrydevv@gmail.com>
2024-05-10 11:52:18 -0300

    novo commit de teste, pra por o conhecimento que tive na parte de fundamentos de java

---
+++ testes-conhecimento/testes.java
@@ -0,0 +1,21 @@
+// Abaixo segue um exemplo de código que você pode ou não utilizar
+import java.util.Scanner;
+
+public class testes {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int A, B, PROD;
+		
+		A = sc.nextInt();
+		B = sc.nextInt();
+		 //TODO: Complete os espaços em branco com uma possível solução para o desafio 
+		PROD = A * B; 
+
+		System.out.println("PROD = " + PROD );
+
+
+
+        sc.close();
+	}
+}
