b6e57c07e2a90d2d805560e4b686fc8ce1a28512
Felipe <felipesalaberrydevv@gmail.com>
2024-05-10 17:34:07 -0300

    novo commit se simplificação de codigo. Fiz uma simplificação de codigo, diminuindo os usos de if/else

--- controle-fluxo/src/ResultadoEscolar.java
+++ controle-fluxo/src/ResultadoEscolar.java
@@ -1,11 +1,27 @@
-public class ResultadoEscolar {
+/*public class ResultadoEscolar {
     public static void main(String[] args) {
-        int nota = 8;
+         int nota = 10;
 
         if(nota >= 7)
         System.out.println("Aprovado");
 
+        else if (nota >= 5 && nota < 7)
+        System.out.println("Prova recuperação");
+
         else
         System.out.println("Reprovado");
+    }
+}*/
+
+//AGORA UMA FORMA MAIS SIMPLIFICADA PRA DIMINUIR IF/ELSE
+
+public class ResultadoEscolar {
+
+    public static void main(String[] args) {
+        int nota = 3;
+
+        String resultado = nota >= 7 ? "Aprovado" :  nota >=5 && nota <7 ? "Recuperação" : "Reprovado" ;
+
+        System.out.println(resultado);
     }
 }
