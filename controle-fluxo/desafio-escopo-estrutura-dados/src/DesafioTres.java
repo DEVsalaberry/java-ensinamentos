5c6bbd33377dfb7d8a91274e8118d8215a81f525
Felipe <felipesalaberrydevv@gmail.com>
2024-05-10 21:13:45 -0300

    correções dos desafios

--- controle-fluxo/desafio-escopo-estrutura-dados/src/DesafioTres.java
+++ controle-fluxo/desafio-escopo-estrutura-dados/src/DesafioTres.java
@@ -2,23 +2,22 @@ import java.util.Scanner;
 
 public class DesafioTres {
     public static void main(String[] args) {
-        @SuppressWarnings("resource")
         Scanner leitor = new Scanner(System.in);
         int C = leitor.nextInt();
         String nome;
-        int N;
+        int forca;
 
         for (int i = 0; i < C; i++) {
             nome = leitor.next();
-            N = leitor.nextInt();
-            if (nome.equals("Thor") && N <= 50) System.out.println("Y");
-            else System.out.println("N");
-        
-        
-
-            
+            forca = leitor.nextInt();
+            if (nome.equals("Thor") && forca <= 5000) {
+                System.out.println("Y");
+            } else {
+                System.out.println("N");
+            }
         }
     }
 }
+
 
 
